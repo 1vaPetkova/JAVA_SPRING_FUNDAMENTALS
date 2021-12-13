205d70c4cb669a55f7f07b5f517ddf4edc0baaf2
Iva Petkova <iva1petkova@gmail.com>
2021-12-13 13:32:52 +0200

    new project for lab_01

---
+++ src/main/java/com/example/java_spring_fund_lab_01/JavaSpringFundLab01Application.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_fund_lab_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringFundLab01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringFundLab01Application.class, args);
+    }
+
+}
