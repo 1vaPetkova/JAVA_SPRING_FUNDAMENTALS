11296c96d197b8958a2e47fd17048843dc8200b2
Iva Petkova <iva1petkova@gmail.com>
2021-12-17 00:05:24 +0200

    added offer in DataInit /with some corrections/

--- src/main/java/com/example/java_spring_fund_lab_01/services/impl/OfferServiceImpl.java
+++ src/main/java/com/example/java_spring_fund_lab_01/services/impl/OfferServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.java_spring_fund_lab_01.services.impl;
 
-import com.example.java_spring_fund_lab_01.models.view.OfferViewDto;
+import com.example.java_spring_fund_lab_01.models.view.OfferSummaryViewDto;
 import com.example.java_spring_fund_lab_01.services.OfferService;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +9,7 @@ import java.util.List;
 @Service
 public class OfferServiceImpl implements OfferService {
     @Override
-    public List<OfferViewDto> getAllOffers() {
+    public List<OfferSummaryViewDto> getAllOffers() {
         return null;
     }
 }
