c7afe260ad8be1312e4549bfab99dd6306063ff9
Iva Petkova <iva1petkova@gmail.com>
2021-12-20 23:56:44 +0200

    updated structure

---
+++ JAVA_SPRING_FUND_PATHFINDER/src/main/java/com/example/java_spring_fund_pathfinder/util/ValidationUtil.java
@@ -0,0 +1,10 @@
+package com.example.java_spring_fund_pathfinder.util;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+public interface ValidationUtil {
+    <E> boolean isValid(E entity);
+
+    <E> Set<ConstraintViolation<E>> violations (E entity);
+}
