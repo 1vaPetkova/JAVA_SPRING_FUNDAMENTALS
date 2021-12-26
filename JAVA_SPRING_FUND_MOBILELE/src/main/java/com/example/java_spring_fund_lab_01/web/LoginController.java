b8df07e7ef92dff8cc834777e0cd7ba4a0d4d5b8
Iva Petkova <iva1petkova@gmail.com>
2021-12-26 23:20:26 +0200

    added user login

--- JAVA_SPRING_FUND_MOBILELE/src/main/java/com/example/java_spring_fund_lab_01/web/LoginController.java
+++ JAVA_SPRING_FUND_MOBILELE/src/main/java/com/example/java_spring_fund_lab_01/web/LoginController.java
@@ -1,21 +1,33 @@
 package com.example.java_spring_fund_lab_01.web;
 
+import com.example.java_spring_fund_lab_01.models.service.UserLoginServiceModel;
 import com.example.java_spring_fund_lab_01.security.CurrentUser;
+import com.example.java_spring_fund_lab_01.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
-    private CurrentUser currentUser;
+    private final UserService userService;
 
-    public LoginController(CurrentUser currentUser) {
-        this.currentUser = currentUser;
+    public LoginController(UserService userService) {
+        this.userService = userService;
     }
 
-    @GetMapping("users/login")
-    public String login() {
+    @GetMapping("/users/login")
+    public String showLogin() {
 
         return "auth-login";
+    }
+
+    @PostMapping("/users/login")
+    public String login(UserLoginServiceModel model) {
+        if (userService.isAuthenticated(model.getUsername(), model.getPassword())) {
+            this.userService.loginUser(model.getUsername());
+            return "redirect:/";
+        }
+        return "redirect:/users/login";
     }
 }
