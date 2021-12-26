d7306df00eea0ea6a489e196d06474a85daeefd0
Iva Petkova <iva1petkova@gmail.com>
2021-12-26 19:32:45 +0200

    amended services

--- JAVA_SPRING_FUND_Lab_01/src/main/java/com/example/java_spring_fund_lab_01/services/UserRoleService.java
+++
@@ -1,4 +0,0 @@
-package com.example.java_spring_fund_lab_01.services;
-
-public interface UserRoleService {
-}
--- JAVA_SPRING_FUND_Lab_01/src/main/java/com/example/java_spring_fund_lab_01/services/impl/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.java_spring_fund_lab_01.services.impl;
-
-import com.example.java_spring_fund_lab_01.services.UserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserService {
-}
