21f1b1fbbe74ebbbd92c9e68484c044344632554
Iva Petkova <iva1petkova@gmail.com>
2021-12-27 16:47:50 +0200

    added users

--- JAVA_SPRING_FUND_MOBILELE/src/main/java/com/example/java_spring_fund_lab_01/App.java
+++ JAVA_SPRING_FUND_MOBILELE/src/main/java/com/example/java_spring_fund_lab_01/App.java
@@ -1,14 +1,7 @@
 package com.example.java_spring_fund_lab_01;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class App {
