65a6b033572d195c4cbb6e3b4e814f3e2162812a
Iva Petkova <iva1petkova@gmail.com>
2021-12-29 14:06:29 +0200

    validations

--- JAVA_SPRING_FUND_MOBILELE/src/main/java/com/example/java_spring_fund_lab_01/models/service/UserLoginServiceModel.java
+++ JAVA_SPRING_FUND_MOBILELE/src/main/java/com/example/java_spring_fund_lab_01/models/service/UserLoginServiceModel.java
@@ -1,16 +1,18 @@
 package com.example.java_spring_fund_lab_01.models.service;
 
-import com.example.java_spring_fund_lab_01.security.CurrentUser;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class UserLoginServiceModel {
+
+    @NotBlank
+    @Size(min = 2)
     private String username;
-    private String password;
 
     @NotBlank
     @Size(min = 3)
+    private String password;
+
     public String getUsername() {
         return username;
     }
@@ -19,8 +21,7 @@ public class UserLoginServiceModel {
         this.username = username;
         return this;
     }
-    @NotBlank
-    @Size(min = 5)
+
     public String getPassword() {
         return password;
     }
