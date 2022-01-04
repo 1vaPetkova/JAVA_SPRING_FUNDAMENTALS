3a54d97c6f9d5b968442ef32f6f4398774bf281a
Iva Petkova <iva1petkova@gmail.com>
2022-01-04 08:15:24 +0200

    revert again 20210104

---
+++ JAVA_SPRING_FUND_PATHFINDER/src/test/java/com/example/java_spring_fund_pathfinder/JavaSpringFundPathfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_fund_pathfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringFundPathfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
