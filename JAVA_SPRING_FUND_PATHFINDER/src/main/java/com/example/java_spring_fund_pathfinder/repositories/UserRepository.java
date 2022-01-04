c508f64c30526467a7703cb1dc6b3ff55c9d83f5
Iva Petkova <iva1petkova@gmail.com>
2022-01-04 08:11:12 +0200

    revert again 20210104

--- JAVA_SPRING_FUND_PATHFINDER/src/main/java/com/example/java_spring_fund_pathfinder/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.java_spring_fund_pathfinder.repositories;
-
-import com.example.java_spring_fund_pathfinder.domain.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
