b998af4103310ea4bdd902f97b0a5846d70d65ff
Iva Petkova <iva1petkova@gmail.com>
2022-01-11 15:29:18 +0200

    adding all routes implementation

--- JAVA_SPRING_FUND_PATHFINDER/src/main/java/com/example/java_spring_fund_pathfinder/services/RouteService.java
+++ JAVA_SPRING_FUND_PATHFINDER/src/main/java/com/example/java_spring_fund_pathfinder/services/RouteService.java
@@ -1,4 +1,9 @@
 package com.example.java_spring_fund_pathfinder.services;
 
+import com.example.java_spring_fund_pathfinder.models.view.RouteViewModel;
+
+import java.util.List;
+
 public interface RouteService {
+    List<RouteViewModel> findAllRoutesView();
 }
--- JAVA_SPRING_FUND_PATHFINDER/src/main/java/com/example/java_spring_fund_pathfinder/services/impl/RouteServiceImpl.java
+++ JAVA_SPRING_FUND_PATHFINDER/src/main/java/com/example/java_spring_fund_pathfinder/services/impl/RouteServiceImpl.java
@@ -1,8 +1,35 @@
 package com.example.java_spring_fund_pathfinder.services.impl;
 
+import com.example.java_spring_fund_pathfinder.models.view.RouteViewModel;
+import com.example.java_spring_fund_pathfinder.repositories.RouteRepository;
 import com.example.java_spring_fund_pathfinder.services.RouteService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RouteServiceImpl implements RouteService {
+
+    private final RouteRepository routeRepository;
+    private final ModelMapper modelMapper;
+
+    public RouteServiceImpl(RouteRepository routeRepository, ModelMapper modelMapper) {
+        this.routeRepository = routeRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public List<RouteViewModel> findAllRoutesView() {
+        return this.routeRepository.findAll()
+                .stream()
+                .map(route -> {
+                    RouteViewModel routeViewModel = this.modelMapper.map(route, RouteViewModel.class);
+                    routeViewModel.setImageUrl(route.getPictures().isEmpty() ?
+                            "/images/pic4.jpg"
+                            : route.getPictures().stream().findFirst().get().getUrl());
+                    return routeViewModel;
+                }).collect(Collectors.toList());
+    }
 }
