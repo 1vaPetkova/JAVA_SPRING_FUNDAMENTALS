a41a62e2f3fb23590d79ea2348a29db40dd32032
Iva Petkova <iva1petkova@gmail.com>
2022-01-12 23:27:57 +0200

    added weather for Sozopol implementation

--- JAVA_SPRING_FUND_PATHFINDER/src/main/resources/static/js/weather.js
+++ JAVA_SPRING_FUND_PATHFINDER/src/main/resources/static/js/weather.js
@@ -12,3 +12,14 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Sofia&appid=8dd1b8c6c706
         boxTempA.innerText = Math.round(info.main.temp - 273.15);
         boxImgA.src = 'images/weather-icons/' + info.weather[0].icon + '.png';
     })
+
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Sozopol&appid=8dd1b8c6c70655b59ef4f75b4d9fb753")
+    .then(data => data.json())
+    .then(info => {
+        console.log(info);
+
+        // Formula Kelvin to Celsius 299K-273.15 = 255.86 C
+        boxTempB.innerText = Math.round(info.main.temp - 273.15);
+        boxImgB.src = 'images/weather-icons/' + info.weather[0].icon + '.png';
+    })
