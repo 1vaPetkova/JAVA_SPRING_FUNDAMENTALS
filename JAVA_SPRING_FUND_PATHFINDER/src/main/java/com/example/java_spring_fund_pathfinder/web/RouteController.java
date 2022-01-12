9967633b3469c03d7d31852f863fd26df92cc700
Iva Petkova <iva1petkova@gmail.com>
2022-01-12 22:13:11 +0200

    adding route details implementation

--- JAVA_SPRING_FUND_PATHFINDER/src/main/java/com/example/java_spring_fund_pathfinder/web/RouteController.java
+++ JAVA_SPRING_FUND_PATHFINDER/src/main/java/com/example/java_spring_fund_pathfinder/web/RouteController.java
@@ -8,10 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
@@ -44,6 +41,14 @@ public class RouteController {
     @ModelAttribute
     public RouteAddBindingModel routeAddBindingModel() {
         return new RouteAddBindingModel();
+    }
+
+
+    @GetMapping("/details/{id}")
+    public String details(@PathVariable Long id){
+
+
+        return "route-details";
     }
 
     @GetMapping("/add")
