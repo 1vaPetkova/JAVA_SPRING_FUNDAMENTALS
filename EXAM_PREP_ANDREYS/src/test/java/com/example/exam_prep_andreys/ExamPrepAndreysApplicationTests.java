48d2dc56038d31e00335becc35864d7142267cfc
Iva Petkova <iva1petkova@gmail.com>
2022-01-18 01:02:38 +0200

    buy products implementation

---
+++ EXAM_PREP_ANDREYS/src/test/java/com/example/exam_prep_andreys/ExamPrepAndreysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exam_prep_andreys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPrepAndreysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- EXAM_PREP_SHOPPING_LIST_APP/src/main/java/com/example/exam_prep_shopping_list_app/services/impl/UserServiceImpl.java
+++ EXAM_PREP_SHOPPING_LIST_APP/src/main/java/com/example/exam_prep_shopping_list_app/services/impl/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.exam_prep_shopping_list_app.services.impl;
 
-import com.example.exam_prep_shopping_list_app.models.binding.UserLoginBindingModel;
 import com.example.exam_prep_shopping_list_app.models.binding.UserRegisterBindingModel;
 import com.example.exam_prep_shopping_list_app.models.entities.User;
 import com.example.exam_prep_shopping_list_app.models.services.UserServiceModel;
@@ -9,8 +8,6 @@ import com.example.exam_prep_shopping_list_app.security.CurrentUser;
 import com.example.exam_prep_shopping_list_app.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -25,9 +22,14 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void registerUser(UserRegisterBindingModel userRegisterBindingModel) {
-        UserServiceModel userServiceModel = this.modelMapper.map(userRegisterBindingModel, UserServiceModel.class);
-        this.userRepository.save(this.modelMapper.map(userServiceModel, User.class));
+    public boolean registerUser(UserRegisterBindingModel userRegisterBindingModel) {
+        try {
+            UserServiceModel userServiceModel = this.modelMapper.map(userRegisterBindingModel, UserServiceModel.class);
+            this.userRepository.save(this.modelMapper.map(userServiceModel, User.class));
+        } catch (Exception e){
+            return false;
+        }
+        return true;
     }
 
     @Override
