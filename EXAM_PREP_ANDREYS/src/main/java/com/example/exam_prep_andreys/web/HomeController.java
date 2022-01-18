751b3af12caa1ae960ec5fd02cb1e55ce888fa7c
Iva Petkova <iva1petkova@gmail.com>
2022-01-18 23:57:06 +0200

    refactoring

--- EXAM_PREP_ANDREYS/src/main/java/com/example/exam_prep_andreys/web/HomeController.java
+++ EXAM_PREP_ANDREYS/src/main/java/com/example/exam_prep_andreys/web/HomeController.java
@@ -2,7 +2,6 @@ package com.example.exam_prep_andreys.web;
 
 import com.example.exam_prep_andreys.services.ItemService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
