c985d537d3a1c371cf25c91c125d0e6040901ae9
Iva Petkova <iva1petkova@gmail.com>
2022-01-19 09:06:07 +0200

    entities init

---
+++ EXAM_PREP_MUSIC_DB/src/main/java/com/example/exam_prep_music_db/models/entities/Album.java
@@ -0,0 +1,127 @@
+package com.example.exam_prep_music_db.models.entities;
+
+import com.example.exam_prep_music_db.models.entities.enums.GengeEnum;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "albums")
+public class Album extends BaseEntity {
+
+    private String name;
+    private String imageUrl;
+    private String description;
+    private Integer copies;
+    private BigDecimal price;
+    private LocalDate releasedDate;
+    private String producer;
+    private GengeEnum genre;
+    private Artist artist;
+    private User addedFrom;
+
+    public Album() {
+    }
+
+    @Column(nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public Album setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Column(name = "image_url", nullable = false)
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Album setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        return this;
+    }
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    public String getDescription() {
+        return description;
+    }
+
+    public Album setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    @Column(nullable = false)
+    public Integer getCopies() {
+        return copies;
+    }
+
+    public Album setCopies(Integer copies) {
+        this.copies = copies;
+        return this;
+    }
+
+    @Column(nullable = false)
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Album setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    @Column(name = "released_date", nullable = false)
+    public LocalDate getReleasedDate() {
+        return releasedDate;
+    }
+
+    public Album setReleasedDate(LocalDate releasedDate) {
+        this.releasedDate = releasedDate;
+        return this;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public Album setProducer(String producer) {
+        this.producer = producer;
+        return this;
+    }
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    public GengeEnum getGenre() {
+        return genre;
+    }
+
+    public Album setGenre(GengeEnum genre) {
+        this.genre = genre;
+        return this;
+    }
+
+    @ManyToOne
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public Album setArtist(Artist artist) {
+        this.artist = artist;
+        return this;
+    }
+
+    @ManyToOne
+    @JoinColumn(name="added_from", referencedColumnName = "id")
+    public User getAddedFrom() {
+        return addedFrom;
+    }
+
+    public Album setAddedFrom(User addedFrom) {
+        this.addedFrom = addedFrom;
+        return this;
+    }
+}
