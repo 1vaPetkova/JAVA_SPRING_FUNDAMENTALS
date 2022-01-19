8e023621640b9f00d4e3aaebb9c75535429328c3
Iva Petkova <iva1petkova@gmail.com>
2022-01-19 08:22:19 +0200

    project init

---
+++ EXAM_PREP_MUSIC_DB/src/main/java/com/example/exam_prep_music_db/MusicDbApp.java
@@ -0,0 +1,13 @@
+package com.example.exam_prep_music_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDbApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicDbApp.class, args);
+    }
+
+}
