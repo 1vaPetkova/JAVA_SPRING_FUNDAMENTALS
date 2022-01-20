268023c7dae75c5fc2bf3c60c2b4a2feb5514cbe
Iva Petkova <iva1petkova@gmail.com>
2022-01-20 12:30:48 +0200

    project init

---
+++ EXAM_PREP_CASEBOOK/src/main/java/com/example/exam_prep_casebook/ExamPrepCasebookApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_prep_casebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPrepCasebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPrepCasebookApplication.class, args);
+    }
+
+}
