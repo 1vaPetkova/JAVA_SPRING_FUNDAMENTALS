7acfe1beafda1ae6a55e0ca3e9548d110a11d82f
Iva Petkova <iva1petkova@gmail.com>
2022-01-21 14:49:15 +0200

    html fragments and project structure

---
+++ EXAM_PREP_CASEBOOK/src/main/java/com/example/exam_prep_casebook/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.exam_prep_casebook.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
