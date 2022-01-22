4bab56c4f0fdd4a79113788b0ff54c8e3c23d2e6
Iva Petkova <iva1petkova@gmail.com>
2022-01-22 14:18:52 +0200

    register implementation

--- EXAM_PREP_CASEBOOK/src/main/java/com/example/exam_prep_casebook/web/HomeController.java
+++ EXAM_PREP_CASEBOOK/src/main/java/com/example/exam_prep_casebook/web/HomeController.java
@@ -2,12 +2,22 @@ package com.example.exam_prep_casebook.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index(HttpSession httpSession, ModelAndView modelAndView) {
+        if (httpSession.getAttribute("user") == null) {
+            modelAndView.setViewName("index");
+        } else {
+//            modelAndView.addObject("totalItems", this.itemService.getTotalItems());
+//            modelAndView.addObject("items", this.itemService.findItems());
+            modelAndView.setViewName("home");
+        }
+        return modelAndView;
     }
 }
