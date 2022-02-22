d4f4287fb8bab66123a1dfe92ae0eb5cea8bba5e
Iva Petkova <iva1petkova@gmail.com>
2022-02-22 21:23:12 +0200

    adding spring security configurations

--- JAVA_SPRING_FUND_MOBILELE/src/main/java/com/example/java_spring_fund_lab_01/config/ApplicationSecurityConfiguration.java
+++ JAVA_SPRING_FUND_MOBILELE/src/main/java/com/example/java_spring_fund_lab_01/config/ApplicationSecurityConfiguration.java
@@ -1,12 +1,13 @@
 package com.example.java_spring_fund_lab_01.config;
 
-import com.example.java_spring_fund_lab_01.services.impl.MobileleleUserServiceImpl;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -21,6 +22,40 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                //  with this line we allow access to all static resources
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                //  the next line allows access to the home page, login page and registration page to everyone
+                .antMatchers("/", "/users/login", "/users/register").permitAll()
+                //  next we forbid all other pages for unauthenticated users
+                .antMatchers("/**").authenticated()
+                .and()
+                //  configure login with HTML login form with two input fields
+                .formLogin()
+                //  our login page is located at http://<<serveraddress>>:<port>/users/login
+                .loginPage("/users/login")
+                //  this is the name of the <input...> in the login form where user enters their username/email
+                //  the value of this input will be presented to our User details service
+                .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+                //  this is the name of the <input...> in the login form where user enters their password
+                .passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
+                //  the place where we should go in case that the login is successful
+                .defaultSuccessUrl("/")
+                //  the place where we should go in case that the login is not successful
+                .failureForwardUrl("/users/login-error")
+                .and()
+                .logout()
+                //  this is the URL which spring will implement for me and will log the user out
+                .logoutUrl("/users/logout")
+                //  where to go after the logout
+                .logoutSuccessUrl("/")
+                // remove the session from the server
+                .invalidateHttpSession(true)
+                //  delete the cookie that references my session
+                .deleteCookies("JSESSIONID")        ;
+
+
         super.configure(http);
     }
 
