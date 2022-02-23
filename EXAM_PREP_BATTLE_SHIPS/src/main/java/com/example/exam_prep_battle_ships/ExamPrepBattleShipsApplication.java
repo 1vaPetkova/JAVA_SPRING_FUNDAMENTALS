8d16b4da80a84559668fb56423e65e28687ec868
Iva Petkova <iva1petkova@gmail.com>
2022-02-23 15:16:15 +0200

    refresh project

---
+++ EXAM_PREP_BATTLE_SHIPS/src/main/java/com/example/exam_prep_battle_ships/ExamPrepBattleShipsApplication.java
@@ -0,0 +1,13 @@
+package com.example.exam_prep_battle_ships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPrepBattleShipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamPrepBattleShipsApplication.class, args);
+    }
+
+}
